eb4119c77c338a095d77ed9c29af18d68f7de668
Arialdo Martini <arialdomartini@gmail.com>
2018-01-28 19:04:05 +0100

    A katana can chop people in 2

---
+++ Samurai/Samurai.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Samurai
+{
+    public class Katana
+    {
+        public string UseAgainst(string target)
+        {
+            return $"I chop you in 2, {target}!";
+        }
+    }
+
+    public class KatanaTest
+    {
+        [Fact]
+        public void should_chop_people_in_two()
+        {
+            var sut = new Katana();
+
+            var result = sut.UseAgainst("Christian");
+
+
+            result.Should().Be("I chop you in 2, Christian!");
+        }
+    }
+}
